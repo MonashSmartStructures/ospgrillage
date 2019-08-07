0792dd6b884064a4a274d4b2a7d1363c6611bd13
MonashSmartStructures <53598771+MonashSmartStructures@users.noreply.github.com>
2019-08-07 16:53:33 +1000

    Delete request_SN3595_demo.py

--- request_SN3595_demo.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-r = requests.get("http://sn3595.ddns.net/#/FileBrowser;component/FileBrowserPage.dyn.xaml")
-
-print(r.text)
-
-
-#with open('Testing.txt','wb') as f:
- #   f.write(r.content)
