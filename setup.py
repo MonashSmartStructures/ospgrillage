a1f7050f3d3af7c786f92f2d60a3b5ad30274bc8
ccaprani <31228546+ccaprani@users.noreply.github.com>
2021-07-30 14:42:28 +1000

    Initial project restructure

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+from ops-grillage import __version__ as version
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="ops-grillage",
+    version=version,
+    description=("A bridge deck grillage wrapper for OpenSeesPy"),
+    license="MIT",
+    keyword="bridge grillage openseespy",
+    author="Monash Smart Structures",
+    author_email="colin.caprani@monash.edu",
+    url="https://monashsmartstructures.github.io/ops-grillage/",
+    packages=find_packages(include=["ops-grillage"]),
+    long_description=read("README"),
+    classifiers=[
+        "Development Status " " 3 - Alpha",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=["openseespy", "xarray"],
+    tests_require=["pytest"],
+)
