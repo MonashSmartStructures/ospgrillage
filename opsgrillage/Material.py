ba7579e2d853e37a190e9f8300a48becafb4fcb0
ccaprani <31228546+ccaprani@users.noreply.github.com>
2021-07-30 19:35:07 +1000

    Added units

--- opsgrillage/Material.py
+++ opsgrillage/Material.py
@@ -1,5 +1,6 @@
 import json
 
+
 class Material:
     """
     Base class for Material objects
@@ -10,19 +11,21 @@ class Material:
         self.op_mat_arg = None
         # assigns variables for all kwargs for specific material types , else sets None
         # properties for Concrete
-        self.fpc = kwargs.get('fpc', None)
-        self.epsc0 = kwargs.get('epsc0', None)
-        self.fpcu = kwargs.get('fpcu', None)
-        self.epsU = kwargs.get('epsU', None)
+        self.fpc = kwargs.get("fpc", None)
+        self.epsc0 = kwargs.get("epsc0", None)
+        self.fpcu = kwargs.get("fpcu", None)
+        self.epsU = kwargs.get("epsU", None)
 
         # properties for Steel
-        self.Fy = kwargs.get('Fy', None)
-        self.E0 = kwargs.get('E0', None)
-        self.b = kwargs.get('b', None)  # strain -hardening ratio.
-        self.a1 = kwargs.get('a1', None)
-        self.a2 = kwargs.get('a2', None)  # isotropic hardening parameter , see Ops Steel01
-        self.a3 = kwargs.get('a3', None)
-        self.a4 = kwargs.get('a4', None)
+        self.Fy = kwargs.get("Fy", None)
+        self.E0 = kwargs.get("E0", None)
+        self.b = kwargs.get("b", None)  # strain -hardening ratio.
+        self.a1 = kwargs.get("a1", None)
+        self.a2 = kwargs.get(
+            "a2", None
+        )  # isotropic hardening parameter , see Ops Steel01
+        self.a3 = kwargs.get("a3", None)
+        self.a4 = kwargs.get("a4", None)
 
 
 class UniAxialElasticMaterial(Material):
@@ -39,11 +42,22 @@ class UniAxialElasticMaterial(Material):
         if self.mat_type == "Concrete01":
             self.op_mat_arg = [self.fpc, self.epsc0, self.fpcu, self.epsU]
         elif self.mat_type == "Steel01":
-            self.op_mat_arg = [self.Fy, self.E0, self.b, self.a1, self.a2, self.a3, self.a4]
+            self.op_mat_arg = [
+                self.Fy,
+                self.E0,
+                self.b,
+                self.a1,
+                self.a2,
+                self.a3,
+                self.a4,
+            ]
         # check if None in entries
         if None in self.op_mat_arg:
             raise Exception(
-                "One or more missing/non-numeric parameters for Material: {} ".format(self.mat_type))
+                "One or more missing/non-numeric parameters for Material: {} ".format(
+                    self.mat_type
+                )
+            )
         return self.mat_type, self.op_mat_arg
 
     def get_uni_mat_ops_commands(self, material_tag):
@@ -51,8 +65,9 @@ class UniAxialElasticMaterial(Material):
         # e.g. concrete01 or steel01
         mat_str = None
         if self.mat_type == "Concrete01" or self.mat_type == "Steel01":
-            mat_str = "ops.uniaxialMaterial(\"{type}\", {tag}, *{vec})\n".format(
-                type=self.mat_type, tag=material_tag, vec=self.op_mat_arg)
+            mat_str = 'ops.uniaxialMaterial("{type}", {tag}, *{vec})\n'.format(
+                type=self.mat_type, tag=material_tag, vec=self.op_mat_arg
+            )
         return mat_str
 
 
@@ -71,64 +86,66 @@ class NDmaterial(Material):
     def get_nd_ops_commands(self):
         pass
 
+
 def create_initial_material_dict():
     """
     Just to make sure the JSON file is formatted correctly 
     Note: 1 ksi = 6.89475728 MPa
     """
 
-    mat_lib = { 'concrete': 
-               {
-                   'AS5100-2017': 
-                   {'25MPa': {'fc':25, 'E': 26.7e9, 'v': 0.2, 'rho': 2.4e3},
-                    '32MPa': {'fc':32, 'E': 30.1e9, 'v': 0.2, 'rho': 2.4e3},
-                    '40MPa': {'fc':40, 'E': 32.8e9, 'v': 0.2, 'rho': 2.4e3},
-                    '50MPa': {'fc':50, 'E': 34.8e9, 'v': 0.2, 'rho': 2.4e3},
-                    '65MPa': {'fc':65, 'E': 37.4e9, 'v': 0.2, 'rho': 2.4e3},
-                    '80MPa': {'fc':80, 'E': 39.6e9, 'v': 0.2, 'rho': 2.4e3},
-                    '100MPa': {'fc':100, 'E': 42.2e9, 'v': 0.2, 'rho': 2.4e3}
-                   },
-                   'AASHTO-LRFD-8th':
-                   {
-                       '2.4ksi': {'fc':16.55, 'E': 23.2223e9, 'v': 0.2, 'rho': 2.4027e3},
-                       '3.0ksi': {'fc':20.68, 'E': 24.997e9, 'v': 0.2, 'rho': 2.4027e3},
-                       '3.6ksi': {'fc':24.82, 'E': 26.547e9, 'v': 0.2, 'rho': 2.4027e3},
-                       '4.0ksi': {'fc':27.58, 'E': 27.486e9, 'v': 0.2, 'rho': 2.4027e3},
-                       '5.0ksi': {'fc':34.47, 'E': 29.587e9, 'v': 0.2, 'rho': 2.4027e3},
-                       '6.0ksi': {'fc':41.37, 'E': 31.856e9, 'v': 0.2, 'rho': 2.4027e3},
-                       '7.5ksi': {'fc':51.71, 'E': 34.999e9, 'v': 0.2, 'rho': 2.4027e3},
-                       '10.0ksi': {'fc':68.95, 'E': 39.8e9, 'v': 0.2, 'rho': 2.4027e3},
-                       '15.0ksi': {'fc':103.42, 'E': 48.582e9, 'v': 0.2, 'rho': 2.4027e3}
-                   }
-                },
-               'steel':
-               {
-                   'AS5100.6-2004':
-                   {
-                       'R250N': {'fy': 250, 'E': 200.0e9, 'v': 0.25, 'rho': 7850},
-                       'D500N': {'fy': 500, 'E': 200.0e9, 'v': 0.25, 'rho': 7850},
-                       'D500L': {'fy': 500, 'E': 200.0e9, 'v': 0.25, 'rho': 7850},
-                   },
-                   'AASHTO-LRFD-8th':
-                   {
-                       'A615-40': {'fy': 275.8, 'E': 200.0e9, 'v': 0.3, 'rho': 7849},
-                       'A615-60': {'fy': 413.67, 'E': 200.0e9, 'v': 0.3, 'rho': 7849},
-                       'A615-75': {'fy': 517.12, 'E': 200.0e9, 'v': 0.3, 'rho': 7849},
-                       'A615-80': {'fy': 551.58, 'E': 200.0e9, 'v': 0.3, 'rho': 7849},
-                       'A615-100': {'fy': 689.48, 'E': 200.0e9, 'v': 0.3, 'rho': 7849}
-                   }
-               }
-            }
-    
-    with open('mat_lib.json', 'w') as f:
+    mat_lib = {
+        "concrete": {
+            "AS5100-2017": {
+                "units": "SI",
+                "25MPa": {"fc": 25, "E": 26.7e9, "v": 0.2, "rho": 2.4e3},
+                "32MPa": {"fc": 32, "E": 30.1e9, "v": 0.2, "rho": 2.4e3},
+                "40MPa": {"fc": 40, "E": 32.8e9, "v": 0.2, "rho": 2.4e3},
+                "50MPa": {"fc": 50, "E": 34.8e9, "v": 0.2, "rho": 2.4e3},
+                "65MPa": {"fc": 65, "E": 37.4e9, "v": 0.2, "rho": 2.4e3},
+                "80MPa": {"fc": 80, "E": 39.6e9, "v": 0.2, "rho": 2.4e3},
+                "100MPa": {"fc": 100, "E": 42.2e9, "v": 0.2, "rho": 2.4e3},
+            },
+            "AASHTO-LRFD-8th": {
+                "units": "SI",
+                "2.4ksi": {"fc": 16.55, "E": 23.2223e9, "v": 0.2, "rho": 2.4027e3},
+                "3.0ksi": {"fc": 20.68, "E": 24.997e9, "v": 0.2, "rho": 2.4027e3},
+                "3.6ksi": {"fc": 24.82, "E": 26.547e9, "v": 0.2, "rho": 2.4027e3},
+                "4.0ksi": {"fc": 27.58, "E": 27.486e9, "v": 0.2, "rho": 2.4027e3},
+                "5.0ksi": {"fc": 34.47, "E": 29.587e9, "v": 0.2, "rho": 2.4027e3},
+                "6.0ksi": {"fc": 41.37, "E": 31.856e9, "v": 0.2, "rho": 2.4027e3},
+                "7.5ksi": {"fc": 51.71, "E": 34.999e9, "v": 0.2, "rho": 2.4027e3},
+                "10.0ksi": {"fc": 68.95, "E": 39.8e9, "v": 0.2, "rho": 2.4027e3},
+                "15.0ksi": {"fc": 103.42, "E": 48.582e9, "v": 0.2, "rho": 2.4027e3},
+            },
+        },
+        "steel": {
+            "AS5100.6-2004": {
+                "units": "SI",
+                "R250N": {"fy": 250, "E": 200.0e9, "v": 0.25, "rho": 7850},
+                "D500N": {"fy": 500, "E": 200.0e9, "v": 0.25, "rho": 7850},
+                "D500L": {"fy": 500, "E": 200.0e9, "v": 0.25, "rho": 7850},
+            },
+            "AASHTO-LRFD-8th": {
+                "units": "SI",
+                "A615-40": {"fy": 275.8, "E": 200.0e9, "v": 0.3, "rho": 7849},
+                "A615-60": {"fy": 413.67, "E": 200.0e9, "v": 0.3, "rho": 7849},
+                "A615-75": {"fy": 517.12, "E": 200.0e9, "v": 0.3, "rho": 7849},
+                "A615-80": {"fy": 551.58, "E": 200.0e9, "v": 0.3, "rho": 7849},
+                "A615-100": {"fy": 689.48, "E": 200.0e9, "v": 0.3, "rho": 7849},
+            },
+        },
+    }
+
+    with open("mat_lib.json", "w") as f:
         json.dump(mat_lib, f, indent=4)
+
 
 def read_mat_lib():
     """
     Just a demo on reading the mat lib
     """
 
-    with open('mat_lib.json', "r") as f:
+    with open("mat_lib.json", "r") as f:
         mat_lib = json.load(f)
 
     # Do something with mat_lib
